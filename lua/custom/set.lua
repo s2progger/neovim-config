154907846d06d9832fb1690baa9ee3360ea5ac04
Simon Twogood <simon.twogood@gmail.com>
2023-09-02 12:52:21 -0600

    More settings

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -16,4 +16,15 @@ vim.opt.backup = false
 vim.opt.scrolloff = 8
 vim.opt.isfname:append("@-@")
 
-
+vim.opt.clipboard = "unnamedplus"  -- Enables the clipboard between Vim/Neovim and other applications.
+vim.opt.completeopt=noinsert,menuone,noselect -- Modifies the auto-complete menu to behave more like an IDE.
+vim.opt.hidden = true -- Hide unused buffers
+vim.opt.autoindent = true -- Indent a new line
+vim.opt.inccommand = "split" -- Show replacements in a split screen
+vim.opt.mouse = "a" -- Allow to use the mouse in the editor
+vim.opt.title = true -- Show file title
+vim.opt.wildmenu = true -- Show a more advance menu
+-- filetype plugin indent on   -- Allow auto-indenting depending on file type
+-- syntax on
+vim.opt.spell = true -- enable spell check (may need to download language package)
+-- vim.opt.ttyfast = true -- Speed up scrolling in Vim
